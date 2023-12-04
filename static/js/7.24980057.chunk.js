eb08b92dd0cebecefa3236a5fd2d419e572f2bc7
AnastasiaGOIT <132286600+AnastasiaGOIT@users.noreply.github.com>
2023-12-04 16:22:23 +0000

    Deploying to gh-pages from @ AnastasiaGOIT/goit-react-hw-05-movies@f37c0ddaaf75cc26dbc23722ef386d3dea0951d7 🚀

---
+++ static/js/7.24980057.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[7],{7:function(e,t,n){n.r(t);var r=n(830),s=n(184);t.default=function(){return(0,s.jsx)(s.Fragment,{children:(0,s.jsx)(r.U,{})})}}}]);
+//# sourceMappingURL=7.24980057.chunk.js.map
